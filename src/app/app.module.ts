e5f1d9ef91f07e0832c1d90f7fb5fc1bb6d3a594
DESKTOP-1ASQ7K6\sunny <sunny.gupta@docquity.com>
2021-04-30 17:58:03 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BasicComponent } from './basic/basic.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { NgxParallaxScrollModule } from 'ngx-parallax-scroll';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxParallaxScrollModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/basic/basic.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+import { ParallaxConf } from "../parallax-config";
+import { IParallaxScrollConfig } from "ngx-parallax-scroll";
+import { animations } from "../animations";
+
+interface Position {
+  left: string;
+  top: string;
+}
+
+interface Star {
+  img: string;
+  position: Position;
+  conf: IParallaxScrollConfig;
+}
+@Component({
+  selector: 'app-basic',
+  templateUrl: './basic.component.html',
+  styleUrls: ['./basic.component.scss'],
+  animations: animations
+
+})
+export class BasicComponent implements OnInit {
+
+ public minStarsCount: number = 10;
+ public maxStarsCount: number = 30;
+  stars: Array<Star> = [];
+
+  ngOnInit() {
+    this.generateStars();
+  }
+
+  generateStars() {
+    for (let index = 0; index < this.generatedRandomStarsCount(); index++) {
+      this.stars.push({
+        img: `https://github.com/farengeyt451/ngx-parallax-scroll/blob/master/src/assets/planet-${Math.round(
+          Math.random() * 4
+        )}.png?raw=true`,
+        position: this.generatePosition(),
+        conf: new ParallaxConf()
+      });
+    }
+  }
+
+  generatePosition(): Position {
+    return {
+      left: `${this.mathRandom(100)}%`,
+      top: `${this.mathRandom(200)}%`
+    };
+  }
+
+  mathRandom(endpoint: number): number {
+    return Math.round(Math.random() * endpoint);
+  }
+
+  generatedRandomStarsCount(): number {
+    return Math.floor(
+      Math.random() * (this.maxStarsCount - this.minStarsCount + 1) +
+        this.minStarsCount
+    );
+  }
+
+  onGalaxyGenerate() {
+    this.stars = [];
+    this.generateStars();
+  }
+
+}
